97bef4cc608b11d456436de66e422c907b448f3f
jonathan-robillard <Jonathan@MacBook-Pro-de-Jonathan.local>
2015-05-07 10:51:07 +0200

    ajout attributs (pas fini)

--- SemiMarathon.java
+++ SemiMarathon.java
@@ -1,21 +1,29 @@
+import java.sql.Date;
+import java.sql.Time;
 import java.util.ArrayList;
 
 
 public class SemiMarathon {
 	
 	private ArrayList<Categorie> categories; 
-	
-	
 	private ArrayList<Partenaire> partenaires; 
-	
-	
 	private ArrayList<Tarif> tarifs; 
-	
-	
 	private ArrayList<TypeDossard> typesDossards; 
+	private ArrayList<Institution> institutions;
 	
+	private String ville;
+	private Date date;
+	private float longueur;
+	private int anneeNaissanceMin;
+	private int nbMaxPart;
+	private String lieuDepArr;
+	private float heureDepart;
+	private Time tempsMax;
+	private String url;
+	private String addInscription;
+	private String lieuRemiseDossard;
+	private Date horairesRemiseDossards[];
 	
-	private ArrayList<Institution> institutions; 
 	
 	
 	
--- Tarif.java
+++ Tarif.java
@@ -1,6 +1,12 @@
+import java.util.ArrayList;
+
 
 public class Tarif {
 
+	private ArrayList<Coureur> coureurs;
+	
+	
+	
 	Tarif()
 	{
 		
